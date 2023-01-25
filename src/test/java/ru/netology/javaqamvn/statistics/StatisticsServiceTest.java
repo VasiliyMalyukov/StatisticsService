6867462fda011dd8b4574552fa6193136cdedf2a
Wovasya <v37444170895@gmail.com>
2023-01-25 19:46:48 +0400

    Modified test

--- src/test/java/ru/netology/javaqamvn/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/javaqamvn/statistics/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 11};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
